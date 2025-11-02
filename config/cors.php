6c900372218190616772ea810a0d30de3bdf77e9
Jorgo69 <ibralejorgo@gmail.com>
2025-11-02 23:01:31 +0100

    second first |

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:4200',
+        'http://127.0.0.1:4200',
+        'https://spacex-dashboard-front-end-angular.vercel.app/',
+        'https://spacex-dashboard-front-end-angular.vercel.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
